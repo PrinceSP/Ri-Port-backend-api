0068b26d2b60930b17de0fdda2656aa03562e351
PrinceSP <princepasombaran@gmail.com>
2021-10-22 09:16:10 +0800

    first time setup the connection

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT || 9000
+
+app.use('/',(req,res)=>res.send('localhost connected'))
+
+app.listen(PORT,()=>console.log(`server connected to port:${PORT}`))
