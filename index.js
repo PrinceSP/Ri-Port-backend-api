be1bb94b49fa5a138feefd1fccb3a7923c461274
PrinceSP <princepasombaran@gmail.com>
2021-11-10 15:50:39 +0800

    change porrt

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 const app = express()
 dotenv.config()
 
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 2000
 
 app.use('/',(req,res)=>res.send('localhost connected'))
 
