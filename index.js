0f830d87bb5d70813da5bf92100cc08971b8b15e
PrinceSP <princepasombaran@gmail.com>
2022-02-22 23:17:54 +0800

    add next function in cross origin resource sharing for let the next controller running

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ dotenv.config()
 const app = express()
 
 //setting up the port
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 //allows express to read the body and then parse that into a Json object
 app.use(bodyparser.json())
 app.use((req,res,next)=>{
   res.setHeader('Access-Control-Allow-Origin','*')
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS')
   res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+  next()
 })
 
 //route middleware
