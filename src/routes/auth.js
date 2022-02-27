05d8845a90243a01bac387f31b4e30149cd6d125
PrinceSP <princepasombaran@gmail.com>
2022-02-27 14:11:22 +0800

    fix error in http headers

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,7 @@
 const router = require('express').Router()
 const authController = require('../controller/auth')
 
-//CREATE -> POST = post the informations when the user register their account
-router.post("/register", authController.register)
+router.post('/register',authController.register)
+router.post('/login',authController.login)
 
-//user login with their registered account
-router.post('/login', authController.login)
 module.exports = router
