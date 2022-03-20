e747a4177e38282e2e0a4c4d3b6b335a7bc19ed4
PrinceSP <princepasombaran@gmail.com>
2022-03-20 18:20:10 +0800

    add mail template html code

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer')
+const bcrypt = require('bcrypt')
 
 module.exports.generateOTP=()=>{
   let otp = ''
@@ -30,4 +31,170 @@ module.exports.mailTransport=()=>{
     }
   })
   return transporter
+}
+
+module.exports.emailTemplate = (OTP)=>{
+  return `<!DOCTYPE html>
+    <html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+
+    <head>
+    <title></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+    <!--[if !mso]><!-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+    <!--<![endif]-->
+    <style>
+    * {
+    box-sizing: border-box;
+    }
+
+    body {
+    margin: 0;
+    padding: 0;
+    }
+
+    a[x-apple-data-detectors] {
+    color: inherit !important;
+    text-decoration: inherit !important;
+    }
+
+    #MessageViewBody a {
+    color: inherit;
+    text-decoration: none;
+    }
+
+    p {
+    line-height: inherit
+    }
+
+    @media (max-width:510px) {
+    .row-content {
+      width: 100% !important;
+    }
+
+    .column .border {
+      display: none;
+    }
+
+    .stack .column {
+      width: 100%;
+      display: block;
+    }
+    }
+    </style>
+    </head>
+
+    <body style="background-color: transparent; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+    <table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: transparent;">
+    <tbody>
+    <tr>
+      <td>
+        <table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+          <tbody>
+            <tr>
+              <td>
+                <table class="row-content" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 490px;" width="490">
+                  <tbody>
+                    <tr>
+                      <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                        <table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td style="width:100%;text-align:center;padding-top:10px;padding-bottom:10px;">
+                              <h1 style="margin: 0; color: #18317c; font-size: 23px; font-family: 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; line-height: 200%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: 1px; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">Verify your email</span></h1>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="row row-2" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+          <tbody>
+            <tr>
+              <td>
+                <table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000000; width: 490px;" width="490">
+                  <tbody>
+                    <tr>
+                      <td class="column column-1" width="100%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; padding-top: 5px; padding-bottom: 5px; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;">
+                        <table class="paragraph_block" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td>
+                              <div style="color:#000000;font-size:14px;font-family:Arial, Helvetica Neue, Helvetica, sans-serif;font-weight:400;line-height:120%;text-align:left;direction:ltr;letter-spacing:0px;">
+                                <p style="margin: 0;">by entering this code below, you have successfully verifying your email for your account in our app</p>
+                              </div>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table class="row row-3" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+          <tbody>
+            <tr>
+              <td>
+                <table class="row-content" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #ebffeb; color: #000000; width: 490px;" width="490">
+                  <tbody>
+                    <tr>
+                      <td class="column column-1" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-bottom: 8px solid #0068A5; border-left: 8px solid transparent; border-right: 8px solid transparent; border-top: 8px solid transparent;">
+                        <table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td style="width:100%;text-align:center;padding-top:5px;padding-bottom:5px;">
+                              <h1 style="margin: 0; color: #393d47; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">${OTP[0]}</span></h1>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                      <td class="column column-2" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-bottom: 8px solid #0068A5; border-left: 8px solid transparent; border-right: 8px solid transparent; border-top: 8px solid transparent;">
+                        <table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td style="width:100%;text-align:center;padding-top:5px;padding-bottom:5px;">
+                              <h1 style="margin: 0; color: #393d47; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">${OTP[1]}</span></h1>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                      <td class="column column-3" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 8px solid transparent; border-right: 8px solid transparent; border-bottom: 8px solid #0068A5; border-left: 8px solid transparent;">
+                        <table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td style="width:100%;text-align:center;padding-top:5px;padding-bottom:5px;">
+                              <h1 style="margin: 0; color: #393d47; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">${OTP[2]}</span></h1>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                      <td class="column column-4" width="25%" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; vertical-align: top; border-top: 8px solid transparent; border-right: 8px solid transparent; border-bottom: 8px solid #0068A5; border-left: 8px solid transparent;">
+                        <table class="heading_block" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+                          <tr>
+                            <td style="width:100%;text-align:center;padding-top:5px;padding-bottom:5px;">
+                              <h1 style="margin: 0; color: #393d47; font-size: 23px; font-family: Arial, Helvetica Neue, Helvetica, sans-serif; line-height: 120%; text-align: center; direction: ltr; font-weight: 700; letter-spacing: normal; margin-top: 0; margin-bottom: 0;"><span class="tinyMce-placeholder">${OTP[3]}</span></h1>
+                            </td>
+                          </tr>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+    </tbody>
+    </table>
+    </body>
+
+    </html>
+    `;
 }
