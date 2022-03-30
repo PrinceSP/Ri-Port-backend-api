d9730e77f5edfdf1eaa3935f44f618630aac1817
PrinceSP <princepasombaran@gmail.com>
2022-03-30 21:15:10 +0800

    add chek phone number route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controller/auth')
 router.post('/register',authController.register)
 router.post('/login',authController.login)
 router.post('/verify-email',authController.verifyEmail)
-router.post('/verify-phoneNumber',authController.verifyPhone)
+router.post('/check-phoneNumber',authController.smsOtpToPhone)
 
 module.exports = router
